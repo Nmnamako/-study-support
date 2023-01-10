5d5ce403a81f57a58ba87e0698b0a43dbd3ff089
EC2 Default User <na.maechan@icloud.com>
2023-01-10 10:58:20 +0000

    [Update]タイマーモーダル,タスクタイトル出力完成

--- study_support/app/controllers/tasks_controller.rb
+++ study_support/app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
   def index
     @user = current_user
     @task = Task.new
-    
+    @taskShow = nil
     #ログインユーザーのみ表示させる
     if user_signed_in?
       @tasks = @user.tasks
@@ -21,8 +21,11 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
   
+  
+  # 部分テンプレートにてshow作成したが不要かもしれない
   def show
-    @task = Task.find(params[:id])
+    @taskShow = nil
+    # @task = Task.find(params[:id])
   end
   
   private
--- study_support/app/javascript/packs/task/index.js
+++ study_support/app/javascript/packs/task/index.js
@@ -1,8 +1,12 @@
 const taskModal = document.getElementById("taskModal");
 const black = document.getElementById("black");
 const timerModal = document.getElementById("timerModal");
+let taskTitleShow = document.getElementById("taskTitleShow")
 
 
+//.innerHTMLをつけることで[object HTMLSpanElement]の出力を回避する
+let taskTitle1 = document.getElementById("taskTitle1").innerHTML;
+let taskTitle2= document.getElementById("taskTitle2").innerHTML;
 
 //タスク作成押下後、タスク作成モーダル展開
 document.getElementById("taskCreation").addEventListener('click', function() {
@@ -12,18 +16,25 @@ document.getElementById("taskCreation").addEventListener('click', function() {
 
 
 document.querySelector(".timerStart1").addEventListener('click', function() {
-  taskModal.classList.add("active");
+  timerModal.classList.add("active");
   black.classList.add("active");
+  taskTitleShow.innerHTML = taskTitle1;
 });
 
 document.querySelector(".timerStart2").addEventListener('click', function() {
-  taskModal.classList.add("active");
+  timerModal.classList.add("active");
   black.classList.add("active");
+  taskTitleShow.innerHTML = taskTitle2;
 });
 
 document.querySelector(".timerStart3").addEventListener('click', function() {
-  taskModal.classList.add("active");
+  timerModal.classList.add("active");
   black.classList.add("active");
+});
+
+document.getElementById("timerModalClose").addEventListener('click', function() {
+  timerModal.classList.remove("active");
+  black.classList.remove("active");
 });
 
 //タイマー開始押下後、タイマーモーダル展開
