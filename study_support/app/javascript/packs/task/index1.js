76eba1138d5b1eb5c3208d3acef365d674ed6924
EC2 Default User <na.maechan@icloud.com>
2023-01-11 13:28:53 +0000

    [Update]ゲスト用タイマー修正完了

---
+++ study_support/app/javascript/packs/task/index1.js
@@ -0,0 +1,89 @@
+
+//カウントダウンタイマー関係
+//時間格納用
+let guestMinTime = 25;
+let guestSecTime = 0;
+
+//タイマー停止用
+let guestInterval;
+
+//falseかtrueかで、作業時間と休憩時間を変更する
+let guestChangeTime = false;
+
+function progressGuestMin() {
+  document.getElementById("guestMin").textContent = guestMinTime + "分";
+};
+
+function progressGuestSin() {
+  document.getElementById("guestSec").textContent = guestSecTime + "秒";
+};
+
+
+progressGuestMin();
+progressGuestSin();
+
+
+function guestStart() {
+  if (guestInterval == null) {
+    guestInterval = setInterval(function(){
+      guestSecTime--;
+      progressGuestSin();
+      if (guestSecTime == -1){
+        guestSecTime = 59;
+        guestMinTime--;
+        progressGuestMin()
+        progressGuestSin();
+        if (guestMinTime == -1 && guestChangeTime == false ){
+          
+          //次の作業のためにtrueに変更
+          guestChangeTime = true;
+          clearInterval(guestInterval);
+          guestReset();
+          guestInterval = null;
+          
+          //alertだが、音で知らせてノーストップで時間を進める予定
+          alert("休憩時間です");
+        } else if (guestMinTime == -1 && guestChangeTime == true) {
+          
+          //次の休憩のためにfalseに変更
+          guestChangeTime = false;
+          clearInterval(guestInterval);
+          guestReset();
+          guestInterval = null;
+          alert("開始時間です");
+        };
+      };
+    },1000);
+  };
+};
+
+
+function guestStop() {
+  clearInterval(guestInterval);
+  guestInterval = null;
+};
+
+
+function guestReset() {
+  if (guestChangeTime == true) {
+    //休憩時間
+    guestMinTime = 5;
+    guestSecTime = 0;
+    progressGuestMin();
+    progressGuestSin();
+  } else {
+    //作業時間
+    guestMinTime = 25;
+    guestSecTime = 0;
+    progressGuestMin();
+    progressGuestSin();
+  };
+};
+
+document.getElementById("guestStart").addEventListener('click', function(){
+  guestStart();
+});
+
+document.getElementById("guestStop").addEventListener('click', function(){
+  guestStop();
+});
