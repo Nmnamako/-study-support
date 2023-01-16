121954b1caabedf0aa5176f5ce37ba76f4a36ff0
EC2 Default User <na.maechan@icloud.com>
2023-01-16 08:37:40 +0000

    [Update]グラフへの値出力完了,値を計算して出力する方法を模索中

--- study_support/app/controllers/users_controller.rb
+++ study_support/app/controllers/users_controller.rb
@@ -5,16 +5,16 @@ class UsersController < ApplicationController
     @times = @user.usage_times
     @task = @user.tasks
     
-    
+    # 作業終了後削除
     @usage_time = UsageTime.all
     
+    
     @today_time = @times.created_today
-    @yesterday_times = @times.created_yesterday.pluck(:elapsed_time)
-
-    
-    
-    
-    pp @today_time
+    @yesterday_times = @times.created_yesterday
+    @two_days_ago_times = @times.created_two_days_ago
+    @three_days_ago_times = @times.created_three_days_ago
+    @four_days_ago_times = @times.created_four_days_ago
+    @five_days_ago_times = @times.created_four_days_ago
+    @six_days_ago_times = @times.created_six_days_ago
   end
-  
 end
