424f57f8b3309316b0faf97e27f1fdb71131c9eb
EC2 Default User <na.maechan@icloud.com>
2023-01-18 02:53:25 +0000

    [Update]taskのbody追加完了,編集機能完了

--- study_support/db/migrate/20230109051947_create_tasks.rb
+++ study_support/db/migrate/20230109051947_create_tasks.rb
@@ -2,7 +2,8 @@ class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
       t.integer "user_id", null: false
-      t.text "title", null: false
+      t.string "title", null: false
+      t.text "body"
 
       t.timestamps
     end
