34d6261fc21bda086f08b2608736984efc85a6a5
EC2 Default User <na.maechan@icloud.com>
2023-01-23 05:42:24 +0000

    [Fix]タイマー注記一部修正

--- study_support/config/routes.rb
+++ study_support/config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   root to: "homes#top"
   
   get "homes/top" => "homes#top"
-  get "homes/my_page" => "homes#my_page"
   get "users/my_page" => "users#my_page"
   
   # ゲストユーザー用
@@ -15,7 +14,7 @@ Rails.application.routes.draw do
   
   
   resources :tasks, only: [:new, :index, :edit, :update, :create, :destroy]
-  resources :usage_times, only: [:new, :create]
+  resources :usage_times, only: [:create]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
