79d7f03f0db68c9b1861f166e323cbfe08c0ce9b
EC2 Default User <na.maechan@icloud.com>
2023-02-13 06:12:37 +0000

    [Update]日付開始日,

--- study_support/db/migrate/20230109051947_create_tasks.rb
+++ study_support/db/migrate/20230109051947_create_tasks.rb
@@ -4,6 +4,8 @@ class CreateTasks < ActiveRecord::Migration[6.1]
       t.integer "user_id", null: false
       t.string "title", null: false
       t.text "body"
+      t.date "start_date"
+      t.date "end_date"
 
       t.timestamps
     end
